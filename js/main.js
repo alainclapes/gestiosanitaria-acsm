5f0099b7db16f6c2d9d8f0560ee4e9769fc836c2
alainclapes <63004306+alainclapes@users.noreply.github.com>
2020-05-22 16:17:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -229,4 +229,12 @@ function ocultaGestioPacients(objecteRebut){
       eleID_divControls.classList.toggle("d-none");
       mostraBotons();
    }
+$(document).ready(function() { /* Cualquier funcionalidad que queramos agregar a la página por medio de jQuery, debe ser incluida cuando el documento está listo para recibir acciones que modifiquen el DOM de la página. */
+  $('.azul').click(function(event) { /* Seleccionamos el elemento que queremos que realice la función */
+    $('body').css('background', '#08c'); /* La función a realizar añadir CSS al body previamente seleccionado */
+  });
+  $('.blanco').click(function(event) { /* Seleccionamos el elemento que queremos que realice la función */
+    $('body').css('background', '#fff'); /* La función a realizar añadir CSS al body previamente seleccionado */
+  });
+});
    
